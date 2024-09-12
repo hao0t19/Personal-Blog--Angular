5a7714b8ac51531234d008bc4daf7ab09d51f0c2
hao0t19 <changhao020619@gmail.com>
2024-09-12 10:44:52 +0800

    add provider for httpclient instead of HttpClientModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    provideHttpClient()  // Provides HttpClient for dependency injection
   ]
 })
 export class CoreModule { }
