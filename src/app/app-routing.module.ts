3f4597302bd00c4887b40cc592200b9d398f5c3d
hao0t19 <changhao020619@gmail.com>
2024-09-12 10:55:18 +0800

    completed route setting in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',pathMatch:'full',redirectTo:'home' }, //empty string will leead to home 
+  { path:'home',component:HomeComponent },
+  { path:'about' , component:AboutComponent },
+  { path:'**' , component:HomeComponent }, //random url will lead to home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
